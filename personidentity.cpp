1d9129f31683119b4ea53fbf2161f159e7c8fae6
MPG1456 <pc.lenovo.mpg@gmail>
2025-06-30 21:33:53 +0330

    PersonIdentity class added with attributes and methodes and constructor.

--- personidentity.cpp
+++ personidentity.cpp
@@ -1,3 +1,68 @@
 #include "personidentity.h"
 
-PersonIdentity::PersonIdentity() {}
+PersonIdentity::PersonIdentity(QString newUsername, QString newPassword,
+                               QString newFirstName, QString newLastName,
+                               QString newPhoneNumber, int newAge) : username(newUsername), password(newPassword),
+                                                                    firstName(newFirstName), lastName(newLastName),
+                                                                    phoneNumber(newPhoneNumber), age(newAge)
+{}
+
+QString PersonIdentity::getUsername()
+{
+    return username;
+}
+
+void PersonIdentity::setUsername(QString newUsername)
+{
+    this->username = newUsername;
+}
+
+QString PersonIdentity::getPassword()
+{
+    return password;
+}
+
+void PersonIdentity::setPassword(QString newPassword)
+{
+    this->password = newPassword;
+}
+
+QString PersonIdentity::getFirstName()
+{
+    return firstName;
+}
+
+void PersonIdentity::setFirstName(QString newFirstName)
+{
+    this->firstName = newFirstName;
+}
+
+QString PersonIdentity::getLastName()
+{
+    return lastName;
+}
+
+void PersonIdentity::setLastName(QString newLastName)
+{
+    this->lastName = newLastName;
+}
+
+QString PersonIdentity::gePhoneNumber()
+{
+    return phoneNumber;
+}
+
+void PersonIdentity::setPhoneNumber(QString newPhoneNumber)
+{
+    this->phoneNumber = newPhoneNumber;
+}
+
+int PersonIdentity::getAge()
+{
+    return age;
+}
+
+void PersonIdentity::setAge(int newAge)
+{
+    this->age = newAge;
+}
--- personidentity.h
+++ personidentity.h
@@ -1,10 +1,32 @@
 #ifndef PERSONIDENTITY_H
 #define PERSONIDENTITY_H
 
+#include <QString>
+
 class PersonIdentity
 {
+private:
+    QString username;
+    QString password;
+    QString firstName;
+    QString lastName;
+    QString phoneNumber;
+    int age;
+
 public:
-    PersonIdentity();
+    PersonIdentity(QString newUsername, QString newPassword, QString newFirstName, QString newLastName, QString newPhoneNumber, int newAge);
+    QString getUsername();
+    void setUsername(QString newUsername);
+    QString getPassword();
+    void setPassword(QString newPassword);
+    QString getFirstName();
+    void setFirstName(QString newFirstName);
+    QString getLastName();
+    void setLastName(QString newLastName);
+    QString gePhoneNumber();
+    void setPhoneNumber(QString newPhoneNumber);
+    int getAge();
+    void setAge(int newAge);
 };
 
 #endif // PERSONIDENTITY_H
