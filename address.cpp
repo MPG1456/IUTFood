d2db9a2c0b4ba664ff070e2aabc9ed1f65f6657a
MPG1456 <pc.lenovo.mpg@gmail>
2025-06-30 22:07:46 +0330

    Address class and attributes and methods and constructor added.

--- address.cpp
+++ address.cpp
@@ -1,3 +1,58 @@
 #include "address.h"
 
-Address::Address() {}
+Address::Address(QString newCountry, QString newCity,
+                 QString newHomeAddress, QString newHomePhone,
+                 long long int newPostalCode) : country(newCountry), city(newCity),
+    homeAddress(newHomeAddress), homePhone(newHomePhone),
+    postalCode(newPostalCode)
+{}
+
+QString Address::getCountry()
+{
+    return country;
+}
+
+void Address::setCountry(QString newCountry)
+{
+    this->country = newCountry;
+}
+
+QString Address::getCity()
+{
+    return city;
+}
+
+void Address::setCity(QString newCity)
+{
+    this->city = newCity;
+}
+
+QString Address::getHomeAddress()
+{
+    return homeAddress;
+}
+
+void Address::setHomeAddress(QString newHomeAddress)
+{
+    this->homeAddress = newHomeAddress;
+}
+
+QString Address::getHomePhone()
+{
+    return homePhone;
+}
+
+void Address::setHomePhone(QString newHomePhone)
+{
+    this->homePhone = newHomePhone;
+}
+
+long long int Address::getPostalCode()
+{
+    return postalCode;
+}
+
+void Address::setPostalCode(long long int newPostalCode)
+{
+    this->postalCode = newPostalCode;
+}
--- address.h
+++ address.h
@@ -1,10 +1,28 @@
 #ifndef ADDRESS_H
 #define ADDRESS_H
 
+#include <QString>
+
 class Address
 {
+private:
+    QString country;
+    QString city;
+    QString homeAddress;
+    QString homePhone;
+    long long int postalCode;
 public:
-    Address();
+    Address(QString newCountry, QString newCity, QString newHomeAddress, QString newHomePhone, long long int newPostalCode);
+    QString getCountry();
+    void setCountry(QString newCountry);
+    QString getCity();
+    void setCity(QString newCity);
+    QString getHomeAddress();
+    void setHomeAddress(QString newHomeAddress);
+    QString getHomePhone();
+    void setHomePhone(QString newHomePhone);
+    long long int getPostalCode();
+    void setPostalCode(long long int newPostalCode);
 };
 
 #endif // ADDRESS_H
