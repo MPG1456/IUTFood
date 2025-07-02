b9215d132eb1b536d722b91bd764ce4dad0af4ac
MPG1456 <pc.lenovo.mpg@gmail>
2025-07-02 19:17:25 +0330

    Score class added and defined completely.

--- score.cpp
+++ score.cpp
@@ -1,3 +1,38 @@
 #include "score.h"
 
-Score::Score() {}
+Score::Score() : counter(0), myScore(-1)
+{}
+
+void Score::resetScore()
+{
+    myScore = -1;
+    counter = 0;
+}
+
+void Score::addScore(float newScore)
+{
+    myScore *= counter;
+    myScore += newScore;
+    counter++;
+    myScore /= counter;
+}
+
+float Score::getScore()
+{
+    return myScore;
+}
+
+void Score::setScore(float newScore)
+{
+    myScore = newScore;
+}
+
+int Score::getCounter()
+{
+    return counter;
+}
+
+void Score::setCounter(int newCounter)
+{
+    counter = newCounter;
+}
--- score.h
+++ score.h
@@ -3,8 +3,19 @@
 
 class Score
 {
+private:
+    int counter;
+    float myScore;
+
 public:
     Score();
+    void resetScore();
+    void addScore(float newScore);
+    float getScore();
+    void setScore(float newScore);
+    int getCounter();
+    void setCounter(int newCounter);
+    //void showScore(); // This should change later. (Showing this straight to IU or not.)
 };
 
 #endif // SCORE_H
